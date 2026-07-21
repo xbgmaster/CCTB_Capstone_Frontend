a41302c60cb19c40b46fb42eb738c6c75dce4945
Luis Roldan <roldanluis022@gmail.com>
2026-07-20 21:22:34 -0700

    added to main class hero-bg

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,15 +1,15 @@
-import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar.jsx';
-import Footer from './Footer.jsx';
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar.jsx";
+import Footer from "./Footer.jsx";
 
 export default function AppLayout() {
-  return (
-    <div className="flex min-h-full flex-col">
-      <Navbar />
-      <main className="flex-1">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className='flex min-h-full flex-col'>
+			<Navbar />
+			<main className='hero-bg flex-1'>
+				<Outlet />
+			</main>
+			<Footer />
+		</div>
+	);
 }
